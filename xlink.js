ca6974f0aef2852255f21db0bc26fb7de06a2959
achenglike <null>
2023-12-05 11:04:29 +0800

    fix:remove error use mixData

--- xlink.js
+++ xlink.js
@@ -3,7 +3,8 @@ async function xlink_process(input){
     data = JSON.parse(data);
     // url, redirect_url, title, html, xhr
     var bodyStr = data.xhr.filter(function(item){return item.url.indexOf('TweetDetail') > 0})[0].responseBody;
-    var mediaEntries = mixData = JSON.parse(bodyStr).data.threaded_conversation_with_injections_v2.instructions[0].entries;
+    var mediaEntries = JSON.parse(bodyStr).data.threaded_conversation_with_injections_v2.instructions[0].entries;
+    console.log(mediaEntries[mediaEntries.length-1].content.itemContent.tweet_results);
     var mixData = mediaEntries[mediaEntries.length-1].content.itemContent.tweet_results.result;
     var postData = mixData.legacy;
     var posterData = mixData.core.user_results.result.legacy;
