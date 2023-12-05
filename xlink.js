488b18063ce21c14bdcf9214c692df87172794f0
achenglike <null>
2023-12-05 11:46:06 +0800

    fix:x could not find focus

--- xlink.js
+++ xlink.js
@@ -3,15 +3,7 @@ async function xlink_process(input){
     data = JSON.parse(data);
     // url, redirect_url, title, html, xhr
     var bodyStr = data.xhr.filter(function(item){return item.url.indexOf('TweetDetail') > 0})[0].responseBody;
-    var mediaEntries = JSON.parse(bodyStr).data.threaded_conversation_with_injections_v2.instructions[0].entries;
-    console.log('sssssssssssssssssssss')
-    console.log(mediaEntries.length);
-    console.log(mediaEntries[mediaEntries.length-1]);
-    console.log(mediaEntries[mediaEntries.length-1].content);
-    console.log(mediaEntries[mediaEntries.length-2].content.itemContent);
-    console.log(mediaEntries[mediaEntries.length-1].content.itemContent);
-    console.log(mediaEntries[mediaEntries.length-1].content.itemContent.tweet_results);
-    console.log('eeeeeeeeeeeeeeeeeeeee')
+    var mediaEntries = JSON.parse(bodyStr).data.threaded_conversation_with_injections_v2.instructions[0].entries.filter(v => v.content.entryType == 'TimelineTimelineItem' && v.entryId.startsWith('tweet-'));
     var mixData = mediaEntries[mediaEntries.length-1].content.itemContent.tweet_results.result;
     var postData = mixData.legacy;
     var posterData = mixData.core.user_results.result.legacy;
